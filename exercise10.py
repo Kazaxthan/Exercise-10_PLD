148598580233325ecc2411a01c86b56a15543817
Kazaxthan <carlo.paule.29@gmail.com>
2024-01-25 02:04:06 +0800

    second commit

--- exercise10.py
+++ exercise10.py
@@ -3,10 +3,10 @@
 #Given two list of numbers, write a program to create a new list such that the new list should contain odd numbers from the first list and even numbers from the second list.
 
 def create_new_list(list1, list2):
-    # Extract odd numbers from the first list
+    #  odd numbers from the first list
     odd_numbers = [num for num in list1 if num % 2 != 0]
 
-    # Extract even numbers from the second list
+    #  even numbers from the second list
     even_numbers = [num for num in list2 if num % 2 == 0]
 
     # Combine the odd numbers from the first list and even numbers from the second list
@@ -14,10 +14,19 @@ def create_new_list(list1, list2):
 
     return new_list
 
-list1 = [10, 20, 25, 30, 35]
-list2 = [40, 45, 60, 75, 90]
+# Get user input 
+user_input1 = input("Enter the first list of numbers separated by spaces: ")
+user_input2 = input("Enter the second list of numbers separated by spaces: ")
+
+# Convert user input into lists of integers
+try:
+    list1 = [int(i) for i in user_input1.split()]
+    list2 = [int(i) for i in user_input2.split()]
+except ValueError:
+    print("Invalid input. Please enter valid lists of numbers.")
+    exit()
 
 # Create the new list based on the given condition
 result_list = create_new_list(list1, list2)
-
+# result
 print(f"The new list is: {result_list}")
